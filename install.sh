9c5cf60ba7e4454e15d252b0bdab37bc0ab8d1f9
guest1 <ag251994@gmail.com>
2018-04-19 19:20:37 +0530

    issue resolved

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 { # this ensures the entire script is downloaded #
+yum install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_6.x | sudo -E bash -
 yum install nodejs
 
-yum install git
-read  -n 1 -p "Input Selection:" mainmenuinput
+# yum install git
+# read  -n 1 -p "Input Selection:" mainmenuinput
 # yum install redis
 # systemctl start redis
 # yum install -y mongodb-org
