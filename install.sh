f1b1566a70aaf506ebf8974dad59cc4ae3d89c5a
guest1 <ag251994@gmail.com>
2018-04-19 19:16:51 +0530

    issue resolved

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 { # this ensures the entire script is downloaded #
-
+curl -sL https://rpm.nodesource.com/setup_6.x | sudo -E bash -
 yum install nodejs
+
 yum install git
-yum install redis
-systemctl start redis
-yum install -y mongodb-org
-npm install forever
+read  -n 1 -p "Input Selection:" mainmenuinput
+# yum install redis
+# systemctl start redis
+# yum install -y mongodb-org
+# npm install forever
 
 
 } # this ensures the entire script is downloaded #
