a6f36c881d3cde5c087f27ce3e7fbee300992146
guest1 <ag251994@gmail.com>
2018-04-20 12:02:12 +0530

    Gulp added

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
 { # this ensures the entire script is downloaded #
-yum install -y gcc-c++ make
-curl -sL https://rpm.nodesource.com/setup_8.x | sudo -E bash -
-yum -y install nodejs
+# yum install -y gcc-c++ make
+# curl -sL https://rpm.nodesource.com/setup_8.x | sudo -E bash -
+# yum -y install nodejs
+#
+# yum -y install git
 
-yum -y install git
+npm install -g gulp
+# npm install -g forever
+
+# read  -n 1 -p "Input Selection:" mainmenuinput
 # yum install redis
 # systemctl start redis
 # yum install -y mongodb-org
