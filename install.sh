5ae6a2205db5fcf83d79b8617615dce628dfe792
abhishek gupta <ag251994@gmail.com>
2021-01-19 12:01:51 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
 
 { # this ensures the entire script is downloaded #
-# yum install -y gcc-c++ make
-# curl -sL https://rpm.nodesource.com/setup_8.x | sudo -E bash -
-# yum -y install nodejs
-#
-# yum -y install git
-
-npm install -g gulp
-# npm install -g forever
-
-# read  -n 1 -p "Input Selection:" mainmenuinput
-# yum install redis
-# systemctl start redis
-# yum install -y mongodb-org
-# npm install forever
-
+sudo su
+sudo yum update -y
+sudo yum install git -y
+curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
+sudo yum install nodejs
+git clone https://github.com/webtaculars-com/email-sender.git
+cd email-sender/
+npm install
 
 } # this ensures the entire script is downloaded #
