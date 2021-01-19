cac6f48be20ba8b2fd620a15dcfdcb8623bcd66b
abhishek gupta <ag251994@gmail.com>
2021-01-19 12:14:30 +0530

    Update Install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,10 @@ sudo yum update -y
 sudo yum install git -y
 curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
 sudo yum install nodejs
-read -n 1 -p "Input Selection:" "mainmenuinput"
+read nodeInput
 git clone https://github.com/webtaculars-com/email-sender.git
-read -n 2 -p "Input Selection:" "mainmenuinput"
+read username
+read password
 cd email-sender/
 npm install
 
