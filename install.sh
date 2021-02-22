12d819da98b6e3c799721e2a0fff9ebf0644a6f0
abhishek gupta <ag251994@gmail.com>
2021-02-22 10:49:21 +0530

    update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo yum update -y
 sudo yum install git -y
 curl -sL https://rpm.nodesource.com/setup_10.x | sudo bash -
 sudo yum  -y install nodejs
-git clone https://github.com/webtaculars-com/email-sender.git
+git clone https://github.com/webtaculars-com/email-sender-sid.git
 cd email-sender/
 npm install
 
